b8135689723fc230086786fe8ab9e01f90242961
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-21 15:36:44 +0700

    Mengubah nilai data siswa berdasarkan input list

--- app/src/main/java/com/example/myarsitekturmvvm_047/viewmodel/SiswaViewModel.kt
+++ app/src/main/java/com/example/myarsitekturmvvm_047/viewmodel/SiswaViewModel.kt
@@ -14,3 +14,9 @@ class SiswaViewModel : ViewModel (){
     private val _statusUI = MutableStateFlow(value = Siswa())
     val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
 
+    fun setSiswa(ls: MutableList<String>){
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(nama=ls[0], gender=ls[1], alamat=ls[2])
+        }
+    }
+}
