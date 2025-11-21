0e8ed0e62e842eee5d58e1124a7486f5fbb03f56
ibnatysalsabila <salsajpr01@gmail.com>
2025-11-21 15:16:12 +0700

    menambahkan object DataJK

---
+++ app/src/main/java/com/example/myarsitekturmvvm_047/model/DataJK.kt
@@ -0,0 +1,10 @@
+package com.example.myarsitekturmvvm_047.model
+
+import com.example.myarsitekturmvvm_047.R
+
+object DataJK {
+    val JenisK = listOf(
+        R.string.laki,
+        R.string.wanita,
+    )
+}
